39ce5ef5aa045dea702226708f485453f30b23f1
RifkiAzwari07 <145946605+RifkiAzwari07@users.noreply.github.com>
2023-12-04 12:56:23 +0800

    TUGAS ALGO PERTAMA

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //membuat sebuah variabel dan tipe data//
+
+        String nama;
+        String nim;
+        String prodi;
+       
+
+        nama = "Rifki Azwari";
+        nim = "23241037";
+        prodi = "PENDIDKAN TEKNOLOGI INFORMASI";
+        
+        
+
+        //mengeprint
+
+        System.out.println("Nama Lengkap :" + nama);
+        System.out.println("Nim Mahasiswa :"+ nim);
+        System.out.println("Prodi :"+ prodi);
+        }
+}
